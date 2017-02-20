0eea46b68058bd2df53a8a2a910e5650b984d1fd
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-02-20 00:21:50 -0300

    Testando o vue resource

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="contatos">
+      <ul>
+        <li v-for="contato in contatos">{{contato.nome}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      contatos: []
+    }
+  },
+  mounted: function () {
+    var lista = [];
+    this.$http.get('http://localhost:3412/contatos').then((response)=>
+    {
+      this.contatos = response.data
+    }, (response) => {
+      console.log('error', response)
+    }
+  );
+  }
+}
+</script>
+
+<style>
+
+</style>
